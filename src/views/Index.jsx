2e8c894395121de6e99381c85c773881f09439b9
satrioed <32364936+satrio13@users.noreply.github.com>
2025-01-19 09:55:22 +0700

    Create Index.jsx

---
+++ src/views/Index.jsx
@@ -0,0 +1,25 @@
+import NotFound from "./404";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import PostIndex from "./posts/Index";
+import PostCreate from "./posts/Create";
+import PostEdit from "./posts/Edit";
+import DownloadIndex from "./download/Index";
+import DownloadCreate from "./download/Create";
+import DownloadEdit from "./download/Edit";
+
+export {
+  NotFound,
+  Home,
+  Navbar,
+  Login,
+  Register,
+  PostIndex,
+  PostCreate,
+  PostEdit,
+  DownloadIndex,
+  DownloadCreate,
+  DownloadEdit,
+};
